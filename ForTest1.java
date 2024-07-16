5ca7d6a085f51c6a00f523a14fedaf8ed148831d
이상연 <165137925+sangyeon08@users.noreply.github.com>
2024-07-17 08:35:30 +0900

    Add files via upload

---
+++ ForTest1.java
@@ -0,0 +1,10 @@
+public class ForTest1 {
+	public static void main(String[] args) {
+		int add = 0;
+			
+		for(int i = 0; i < 10; i++ ) {
+			add += i;
+		}
+		System.out.println("1부터 10까지의 합은: \t"+add);
+	}
+}
