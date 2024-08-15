8f0d311c92a840a1b132dbe6bc06f85f994a054c
이상연 <165137925+sangyeon08@users.noreply.github.com>
2024-08-16 00:22:42 +0900

    ForTest4.java

--- ForTest4.java
+++ ForTest4.java
@@ -7,12 +7,12 @@ public class ForTest4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("10 이하의 정수를 입력하세요 : ");
+		System.out.print("10\t이하의\t정수를\t입력하세요\t:\t");
 		int user = sc.nextInt();
 		String a = "MSG";
 		
 		if(user>10) {
-			System.out.println("해당 범위에 값이 들어있지 않습니다.");
+			System.out.println("해당\t범위에\t값이\t들어있지\t않습니다.");
 		}else {
 			for(int i = 0; i < user; i++) {
 				System.out.println(a);
