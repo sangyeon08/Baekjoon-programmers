1dbabb5959c1854c1c6c09b97d96f387c9b3af8d
이상연 <165137925+sangyeon08@users.noreply.github.com>
2025-05-23 15:31:26 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,15 @@
+A = int(input())
+
+if A >= 90 :
+    answer = "A"
+elif A >= 80 :
+    answer = "B"
+elif A >= 70 :
+    answer = "C"
+elif A >= 60 :
+    answer = "D"
+else :
+    answer = "F"
+    
+print(answer)
+    
