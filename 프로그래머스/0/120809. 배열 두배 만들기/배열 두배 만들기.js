b6b4a2eb652b5e308956df9755d4ffa954042e5a
이상연 <165137925+sangyeon08@users.noreply.github.com>
2025-10-05 20:21:29 +0900

    [level 0] Title: 배열 두배 만들기, Time: 2.16 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,7 +1,3 @@
 function solution(numbers) {
-    var answer = [];
-    for(let i = 0; i < numbers.length; i++) {
-        answer.push(numbers[i]*2)
-    }
-    return answer;
+    return numbers.reduce((a, c) => [...a, c *2 ], []);
 }
