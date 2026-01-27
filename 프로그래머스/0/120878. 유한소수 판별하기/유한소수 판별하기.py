81cf516cdef9e3663acaccc8e13b7cfe69d1d0b1
이상연 <165137925+sangyeon08@users.noreply.github.com>
2026-01-28 00:02:39 +1100

    [level 0] Title: 유한소수 판별하기, Time: 0.15 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -0,0 +1,18 @@
+import math
+
+def solution(a, b):
+    gcd = math.gcd(a, b)
+    b //= gcd
+    
+    num = []
+    i = 2
+    while i <= b: 
+        if b % i == 0:
+            b //= i
+            num.append(i)
+        else:
+            i += 1
+    
+    if all(i in [2,5] for i in num): 
+        return 1
+    return 2
