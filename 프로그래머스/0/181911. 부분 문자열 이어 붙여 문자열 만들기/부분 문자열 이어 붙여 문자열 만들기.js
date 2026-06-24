36f2d1f74d2b336a677b5e7376508f4c75d2e666
이상연 <165137925+sangyeon08@users.noreply.github.com>
2026-06-24 23:35:35 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.14 ms, Memory: 44.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,10 @@
+function solution(my_strings, parts) {
+    var answer = '';
+    
+    for (let i = 0; i < my_strings.length; i++) {
+        const [s, e] = parts[i];
+        answer += my_strings[i].slice(s, e + 1);
+    }
+    
+    return answer;
+}
